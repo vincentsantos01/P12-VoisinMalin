4e92ba13f104bd0cd5e0bb153f5720e7a41db55d
vincent santos <vincentsantos@outlook.fr>
2021-07-03 14:51:04 +0200

    maj ui

--- VoisinMalin/VoisinMalin/Controller/AddSearchController.swift
+++ VoisinMalin/VoisinMalin/Controller/AddSearchController.swift
@@ -1,8 +1,18 @@
 //
-//  AddSearch.swift
+//  AddSearchController.swift
 //  VoisinMalin
 //
 //  Created by vincent on 03/07/2021.
 //
 
 import UIKit
+
+class AddSearchController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
